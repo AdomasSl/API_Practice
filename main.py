0e4567e893b25f1e5dd11a454e674ada445bdb39
AdomasSl <adomas.slusnys@gmail.com>
2026-01-04 15:58:11 +0200

    final working spaghetti code, getting 5 random locations and saving to file

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 from colorama import Fore, Back, Style
 import requests
 import json
+import random
+from datetime import datetime
+from pathlib import Path
 print()
 print(Fore.CYAN + "-----Select an option-----" + Style.RESET_ALL)
 endpoint = ['Locations']
@@ -16,10 +19,10 @@ match selected:
     case _:
         print('Invalid selection')
         exit()
-
+x = random.randint(1,1065)
 BaseUrl = 'https://pokeapi.co/api/v2/'
 final = BaseUrl + selectedClean
-settings = {"limit": 5, "offset":10}
+settings = {"limit": 5, "offset":x}
 response1 = requests.get(final, params = settings)
 
 print(response1.status_code)
@@ -32,9 +35,17 @@ dicts = data_json['results']
 for item in dicts:
     detail = requests.get(item['url']).json()
     region = detail.get('region')
+    item['region'] = region['name'] if region else None
     print(f'Name: ' + item['name']+' URL: ' + item['url'] + ' Region: '+region['name'])
+current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+output_dir = Path("TEST_to_json")
+output_dir.mkdir(exist_ok=True)
+file_path = output_dir / f"Data_json_{current_datetime}.json"
+with open(file_path, 'w', encoding='utf-8') as f:
+    json.dump(data_json, f, ensure_ascii=False, indent=4)
 
-
+print("file has been closed and saved")
+print()
 
 
 
